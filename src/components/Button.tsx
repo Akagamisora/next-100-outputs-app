c8b3b85fd9e7f3d65ca8d7d9d0cacac3c4f578e7
Akagamisora <akagamisora@gmail.com>
2025-11-17 14:33:04 +0900

    014完了

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,20 @@
 type ButtonProps = {
   label: string;
+  color?: "red" | "blue" | "green";
 };
 
 export default function Button(props: ButtonProps) {
-  const { label } = props;
+  const { label, color } = props;
+
+  let bgColorClass = "bg-blue-500";
+  if (color === "red") {
+    bgColorClass = "bg-red-500";
+  } else if (color === "green") {
+    bgColorClass = "bg-green-500";
+  }
 
   return (
-    <button className="bg-blue-500 text-white px-4 py-2 rounded">
+    <button className={`${bgColorClass} text-white px-4 py-2 rounded`}>
       {label}
     </button>
   );
