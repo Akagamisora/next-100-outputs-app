7bda4c8a59723ae2aad616b7cd205988f16c510f
Akagamisora <akagamisora@gmail.com>
2025-11-19 15:19:41 +0900

    041完了

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 type Props = {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-export default async function PostPage({ params }: Props) {
+export default async function PostPage({ params, searchParams }: Props) {
   const { id } = await params;
+  const { sort } = await searchParams;
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">Post Details</h1>
@@ -13,6 +15,8 @@ export default async function PostPage({ params }: Props) {
       <p className="mt-4 text-xl">
         記事ID: <span className="text-blue-600 font-bold">{id}</span>
       </p>
+
+      <p className="mt-2 text-lg text-gray-600">Sort Query: {sort}</p>
     </main>
   );
 }
